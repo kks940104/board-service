e67cefffed3b688903163ddb43bd5240b6a5180a
kks940104 <kks940104@gmail.com>
2025-01-21 18:02:10 +0900

    .

--- src/main/java/org/anonymous/BoardServiceApplication.java
+++ src/main/java/org/anonymous/BoardServiceApplication.java
@@ -9,5 +9,4 @@ public class BoardServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BoardServiceApplication.class, args);
 	}
-
 }
---
+++ src/main/java/org/anonymous/board/controllers/CommentController.java
@@ -0,0 +1,12 @@
+package org.anonymous.board.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/comment")
+@RequiredArgsConstructor
+public class CommentController {
+
+}
