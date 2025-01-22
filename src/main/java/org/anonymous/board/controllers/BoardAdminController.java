1296d2bad98439f33202172348f1edd7ab1900a4
kks940104 <kks940104@gmail.com>
2025-01-22 17:00:52 +0900

    .

--- src/main/java/org/anonymous/board/controllers/BoardAdminController.java
+++ src/main/java/org/anonymous/board/controllers/BoardAdminController.java
@@ -1,7 +1,12 @@
 package org.anonymous.board.controllers;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.anonymous.board.validators.BoardConfigValidator;
+import org.anonymous.global.exceptions.BadRequestException;
+import org.anonymous.global.libs.Utils;
 import org.anonymous.global.rests.JSONData;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,13 +16,23 @@ import java.util.List;
 @RequiredArgsConstructor
 public class BoardAdminController {
 
+    private final Utils utils;
+    private final BoardConfigValidator configValidator;
+
+
     /**
      * 게시판 설정 등록, 수정 처리
      *
      * @return
      */
     @PostMapping("/config")
-    public JSONData save() {
+    public JSONData save(@Valid @RequestBody RequestConfig form, Errors errors) {
+
+        configValidator.validate(form, errors);
+
+        if (errors.hasErrors()) {
+            throw new BadRequestException(utils.getErrorMessages(errors));
+        }
 
         return null;
     }
