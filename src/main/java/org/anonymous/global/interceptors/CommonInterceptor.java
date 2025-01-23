f118ae8c91b9780cd8b772b8e5b2b4d8d03ca146
kks940104 <kks940104@gmail.com>
2025-01-23 16:46:52 +0900

    .

--- src/main/java/org/anonymous/global/interceptors/CommonInterceptor.java
+++ src/main/java/org/anonymous/global/interceptors/CommonInterceptor.java
@@ -1,9 +1,14 @@
 package org.anonymous.global.interceptors;
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.Objects;
+import java.util.UUID;
 
 @Component
 public class CommonInterceptor implements HandlerInterceptor {
@@ -11,6 +16,44 @@ public class CommonInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
+        // region 사용자 구분 목적의 hash 생성
+
+        String key = "userHash";
+        String userKey = "" + Objects.hash(key);
+        String userHash = null;
+
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals(userKey)) {
+                    userHash = cookie.getValue();
+                    break;
+                }
+            }
+        }
+
+        userHash = StringUtils.hasText(userHash) ? userHash : UUID.randomUUID().toString();
+        response.setHeader("Set-Cookie", String.format("%s=%s; path=/; httpOnly; SameSite=None; Secure", userKey, userHash));
+
+        // endregion
+
         return true;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
